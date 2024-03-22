1d02e37d29f10be3c3c5f4b241d0ad78f5d3f157
Louis Lanoy <64585726+Peanut-XIV@users.noreply.github.com>
2024-03-22 15:59:38 +0100

    Added resources module for icons and placeholders

--- python/Sashimi/sashimi/configuration/configuration.py
+++ python/Sashimi/sashimi/configuration/configuration.py
@@ -16,25 +16,6 @@ class Configuration(BaseModel):
     stage: StageConfiguration = field(default_factory=StageConfiguration)
     scanner: ScannerConfiguration = field(default_factory=ScannerConfiguration)
 
-    def update_z_correction_terms(self, index, blz=None):
-        # TODO: this function should be moved somewhere else
-        # supposes the scan surface is flat and non-vertical
-        fl, br = self.scanner.zones[index].FL, self.scanner.zones[index].BR
-        x, y, z = 0, 1, 2
-
-        if br[x] == fl[x] or br[y] == fl[y]:
-            print("brx == flx or bry == fly !!!")
-            return
-
-        if blz is None:
-            blz = (fl[z] + br[z])//2
-        
-        dz_dx = (blz - fl[z]) / (br[x] - fl[x])
-        dz_dy = (br[z] - blz) / (br[y] - fl[y])
-
-        self.scanner.zones[index].BL_Z = blz
-        self.scanner.zones[index].Z_corrections = [dz_dx, dz_dy]
-
     def save_default(self):
         self.save(os.path.join(os.path.expanduser("~"), ".sashimi", "config.json"), 4)
 
